14de5706e5a8c5cd4b613f52ec39587fb4b8a92c
Scott Carpenter <scottcarpenter01@gmail.com>
2016-12-09 23:39:53 +0000

    adding the koala-bear, koala's can have ties

--- main.go
+++ main.go
@@ -6,11 +6,23 @@ import (
 
 func main() {
 	bear := newBearStruct("Yogi", 42)
+	koala := newKoalaBearStruct("Gary", 21, true, true)
 	fmt.Println("Welcome to the bears application")
 	fmt.Println("Our bear:", *bear)
+	fmt.Println("Our koala:", *koala)
 	fmt.Println("--------------------------------")
 	bear.printDetails()
 	bear.printHatStatus()
+	fmt.Println("--------------------------------")
+	koala.printDetails()
+	koala.printHatStatus()
+	koala.printTieStatus()
+}
+
+//koala bear structure - koala bears can have a hat and a tie !
+type koalaBearStruct struct {
+	hasTie bool
+	bearStruct
 }
 
 //bear structure - bears can have a hat !
@@ -24,6 +36,16 @@ type bearStruct struct {
 type animalStruct struct {
 	name string
 	age  int
+}
+
+//yes, I am fully aware that a koala is a marsupial and not a bear.
+func newKoalaBearStruct(name string, age int, tie bool, hat bool) *koalaBearStruct {
+	koala := koalaBearStruct{}
+	koala.age = age
+	koala.name = name
+	koala.hasHat = hat
+	koala.hasTie = tie
+	return &koala
 }
 
 //this is a constructor function for a bear
@@ -44,4 +66,9 @@ func (bear *bearStruct) printHatStatus() {
 func (animal *animalStruct) printDetails() {
 	fmt.Println("Name:", animal.name)
 	fmt.Println("Age:", animal.age)
+}
+
+//print out the tie status
+func (koala *koalaBearStruct) printTieStatus() {
+	fmt.Println("Does the koala have a tie? ", koala.hasTie)
 }

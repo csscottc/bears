9467d36622f8209c3c2fd3be17357b440cab2393
Scott Carpenter <scottcarpenter01@gmail.com>
2016-12-09 23:07:47 +0000

    Added age field to bear, printing out the bear!

--- main.go
+++ main.go
@@ -8,17 +8,20 @@ func main() {
 	bear := newBearStruct("Yogi")
 	fmt.Println("Welcome to the bears application")
 	bear.printName()
+	fmt.Println(*bear)
 }
 
 //This is the definition of the bear structure
 type bearStruct struct {
 	name string
+	age  int
 }
 
 //This is a constructor function for a bear
 func newBearStruct(name string) *bearStruct {
 	result := bearStruct{} //Create an instance
 	result.name = name
+	result.age = 42
 	return &result //de-reference (Return the bear, not the memory address)
 }
 

328ed0966d4a2adc9869d4477e66c81c025fa613
Scott Carpenter <scottcarpenter01@gmail.com>
2016-12-09 22:19:55 +0000

    Giving a bear a name via a constructor function

--- main.go
+++ main.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main() {
+	bear := newBearStruct("Yogi")
 	fmt.Println("Welcome to the bears application")
+	fmt.Println(bear.name)
+}
+
+//This is the definition of the bear structure
+type bearStruct struct {
+	name string
+}
+
+//This is a constructor function for a bear
+func newBearStruct(name string) *bearStruct {
+	result := bearStruct{} //Create an instance
+	result.name = name
+	return &result //de-reference
 }
